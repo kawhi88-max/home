f1e9a8b435fea8effaedf39a66f02e9db044cfbf
kahi88-max <mjumabaev22@gmail.com>
2021-04-02 22:38:30 +0600

    Name Of commit

---
+++ Homework 2/Homework 2/src/com/company/Main.java
@@ -0,0 +1,16 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+        System.out.println(ageOfPerson());
+        System.out.println(temperature());
+    }
+    public static int ageOfPerson(){
+        return 35 - 20;
+    }
+    public static int temperature(){
+        return 10 + 12;
+    }
+
+}
